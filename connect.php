866c5a233f79ec8dc168188d707b1be68ac35b49
Virza <virzarizky151@gmail.com>
2025-09-09 07:16:46 +0700

    konfigurasi connect.php

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "daftar_perangkat_lunak";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+?>
